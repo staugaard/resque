338d839058aa73b6aa2226665fac33f095ee8736
Chris Wanstrath <chris@ozmm.org>
2010-03-23 16:34:03 -0700

    Revert "Use `around_perform` hook, change into module."
    This reverts commit 4f686cf2bd486fd5d48fe9a407ad6ade707ed1d8.

--- lib/resque/jobs/locked.rb
+++ lib/resque/jobs/locked.rb
@@ -1,35 +1,32 @@
 module Resque
   module Jobs
     # If you want only one instance of your job running at a time,
-    # extend your class with this module and that's it!
+    # inherit rom this class and define a `perform_without_lock`
+    # method (as opposed to `perform`) at the class level.
     #
     # For example:
     #
-    # class UpdateNetworkGraph
-    #   extend Resque::Jobs::Locked
-    #
-    #   def self.perform(repo_id)
+    # class UpdateNetworkGraph < Resque::Jobs::Locked
+    #   def self.perform_without_lock(repo_id)
     #     heavy_lifting
     #   end
     # end
     #
     # While other UpdateNetworkGraph jobs will be placed on the queue,
-    # the Locked module will check Redis to see if any others are
+    # the Locked class will check Redis to see if any others are
     # executing with the same arguments before beginning. If another
     # is executing the job will be aborted.
     #
     # If you want to define the key yourself you can override the
     # `lock` class method in your subclass, e.g.
     #
-    # class UpdateNetworkGraph
-    #   extend Resque::Jobs::Locked
-    #
+    # class UpdateNetworkGraph < Resque::Jobs::Locked
     #   # Run only one at a time, regardless of repo_id.
     #   def self.lock(repo_id)
     #     "network-graph"
     #   end
     #
-    #   def self.perform(repo_id)
+    #   def self.perform_without_lock(repo_id)
     #     heavy_lifting
     #   end
     # end
@@ -38,21 +35,28 @@ module Resque
     # UpdateNetworkGraph is running at a time, regardless of the
     # repo_id. Normally a job is locked using a combination of its
     # class name and arguments.
-    module Locked
-      # Override in your job class to control the lock key. It is
-      # passed the same arguments as `perform`.
-      def lock(*args)
+    class Locked
+      # Override in your subclass to control the lock key. It is
+      # passed the same arguments as `perform_without_lock`, that is,
+      # your job's payload.
+      def self.lock(*args)
         "locked:#{name}-#{args.to_s}"
       end
 
       # Convenience method, not used internally.
-      def locked?
+      def self.locked?
         Resque.redis.exist(lock)
       end
 
       # Do not override - this is where the magic happens. Instead
       # provide your own `perform_without_lock` class level method.
-      def around_perform(*args)
+      def self.perform(*args)
+        with_lock do
+          perform_without_lock(*args)
+        end
+      end
+
+      def self.with_lock
         # Abort if another job has created a lock.
         return unless Resque.redis.setnx(lock, true)
 
